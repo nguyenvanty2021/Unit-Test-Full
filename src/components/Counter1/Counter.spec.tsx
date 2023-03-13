b2774416c27a5709c62f660748ec3b4acecf873d
Steven Cao <anhhung_so11@yahoo.com>
2023-03-13 13:56:52 +0700

    update

--- src/components/Counter1/Counter.spec.tsx
+++ src/components/Counter1/Counter.spec.tsx
@@ -1,4 +1,9 @@
-import { render, screen } from "@testing-library/react";
+import {
+  render,
+  screen,
+  waitFor,
+  waitForElementToBeRemoved,
+} from "@testing-library/react";
 import { Counter } from "./Counter";
 import user from "@testing-library/user-event";
 
@@ -18,22 +23,32 @@ describe("Counter", () => {
     });
 
     describe('when the incrementor changes to 5 and "+" button is clicked', () => {
-      beforeEach(() => {
+      beforeEach(async () => {
         user.type(screen.getByLabelText(/Incrementor/), "{selectall}5");
         user.click(screen.getByRole("button", { name: "Add to Counter" }));
+        await screen.findByText("Current Count: 15");
       });
 
       it('renders "Current Count: 15"', () => {
         expect(screen.getByText("Current Count: 15")).toBeInTheDocument();
       });
 
+      // Documentation: https://testing-library.com/docs/guide-disappearance/#waiting-for-disappearance
+      // eslint-disable-next-line jest/expect-expect
+      it('"I am too small" disappears after 300ms', async () => {
+        await waitForElementToBeRemoved(() =>
+          screen.queryByText("I am too small")
+        );
+      });
+
       describe('when the incrementor changes to empty string and "+" button is clicked', () => {
-        beforeEach(() => {
+        beforeEach(async () => {
           user.type(
             screen.getByLabelText(/Incrementor/),
             "{selectall}{delete}"
           );
           user.click(screen.getByRole("button", { name: "Add to Counter" }));
+          await screen.findByText("Current Count: 16");
         });
 
         it('renders "Current Count: 16"', () => {
@@ -87,8 +102,8 @@ describe("Counter", () => {
         user.click(screen.getByRole("button", { name: "Add to Counter" }));
       });
 
-      it('renders "Current count: 1"', () => {
-        expect(screen.getByText("Current Count: 1")).toBeInTheDocument();
+      it('renders "Current count: 1"', async () => {
+        await screen.findByText("Current Count: 1");
       });
     });
   });
--- src/components/Counter1/Counter.tsx
+++ src/components/Counter1/Counter.tsx
@@ -1,4 +1,4 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 
 export interface CounterProps {
   description: string;
@@ -8,6 +8,37 @@ export interface CounterProps {
 export function Counter({ description, defaultCount }: CounterProps) {
   const [count, setCount] = useState(defaultCount);
   const [incrementor, setIncrementor] = useState(1);
+  const [bigEnough, setBigEnough] = useState(defaultCount >= 15);
+
+  useEffect(() => {
+    let id: NodeJS.Timeout;
+
+    if (count >= 15) {
+      id = setTimeout(() => setBigEnough(true), 300);
+    }
+
+    return function cleanup() {
+      clearTimeout(id);
+    };
+  });
+
+  // You have both versions :)
+  //
+  // useEffect(() => {
+  //   let active = true;
+
+  //   if (count >= 15) {
+  //     setTimeout(() => {
+  //       if (active) {
+  //         setBigEnough(true);
+  //       }
+  //     }, 300);
+  //   }
+
+  //   return function cleanup() {
+  //     active = false;
+  //   }
+  // });
 
   return (
     <div>
@@ -33,10 +64,11 @@ export function Counter({ description, defaultCount }: CounterProps) {
       Current Count: {count}
       <button
         aria-label="Add to Counter"
-        onClick={() => setCount(count + incrementor)}
+        onClick={() => setTimeout(() => setCount(count + incrementor), 200)}
       >
         +
       </button>
+      {bigEnough ? null : <div>I am too small</div>}
     </div>
   );
 }
